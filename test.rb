25bf5a9676e884999532c91aa6f1828c93aaf2eb
vaujan <vaujan@github.com>
2026-06-12 18:06:10 +0900

    Hello world

---
+++ test.rb
@@ -0,0 +1,20 @@
+# Variables
+full_name = "Ahmad Fauzan"
+age = 24
+test_array = [1, 2, 3]
+GRAVITY = 0.98 # const
+
+# Function
+def test_function
+end
+
+# do/end block
+test_array.each {|i| test_function }
+
+# Class 1
+class TestClassNaming
+end
+
+# Class 2
+class TestClassNaming
+end
