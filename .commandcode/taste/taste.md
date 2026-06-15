5a1c6f8b0346e72b1c7148df2fb80afc447c7233
vaujan <vaujan@example.com>
2026-06-16 02:09:40 +0900

    chore

---
+++ .commandcode/taste/taste.md
@@ -0,0 +1,7 @@
+# Taste (Continuously Learned by [CommandCode][cmd])
+
+[cmd]: https://commandcode.ai/
+
+# learning-style
+- When user is learning/studying, explain concepts and guide but do not write the code for them. Confidence: 0.85
+
--- test.rb
+++ test.rb
@@ -6,15 +6,17 @@ GRAVITY = 0.98 # const
 
 # Function
 def test_function
+  puts "hello, test"
 end
 
 # do/end block
-test_array.each {|i| test_function }
+test_array.each {|i| puts i }
 
 # Class 1
 class TestClassNaming
 end
 
-# Class 2
-class TestClassNaming
+# Class 3
+class AnotherClassNaming
 end
+
